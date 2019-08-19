65a9d30f76a7e5abf051de88c1bf0316e85a435d
vikas yadav <vikas@vikass-MacBook-Pro.local>
2019-08-20 01:37:07 +0800

    moving to previous setting of d3

--- src/index.js
+++ src/index.js
@@ -4,13 +4,10 @@ let data = require('../my_weather_data.json');
 
 import { LineChart } from '@/app/line-chart';
 
-
-let linechart = new LineChart();
-   linechart.draw(data);
-// d3.json("./my_weather_data.json").then(dataset => {
-//     let linechart = new LineChart();
-//     linechart.draw(dataset);
-// })
+d3.json("./my_weather_data.json").then(dataset => {
+    let linechart = new LineChart();
+    linechart.draw(dataset);
+})
 
 
 
